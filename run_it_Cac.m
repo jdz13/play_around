6a0147c0ba796c43489ee5100b25ab9ad7b2ec80
jdz13 <jdz25@cam.ac.uk>
2020-03-27 11:18:59 +0000

    addition of fwhm doubled variable. looks like a very interesting test actually. WIll need more testing (FWHM)

--- run_it_Cac.m
+++ run_it_Cac.m
@@ -1,8 +1,10 @@
 %% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
 
+clear
+
 tic
 
-theta = linspace(0,pi/2,91); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+theta = linspace(0,pi/2,181); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
 
 KRV = [5,4,3,2.5,2]; % Key ratio values, how strict of a condition do we want 
 RES = [0.15,0.2,0.25,0.3,0.35,0.4]; % Start field values. 
@@ -10,12 +12,12 @@ RES = [0.15,0.2,0.25,0.3,0.35,0.4]; % Start field values.
 pm_cl = [2,3,4]*1e-2; % Magnet outer diameters.
 
 Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
-Zin = linspace(-1e-3, 1e-3,101); % Probe plane points in Z 
+Zin = linspace(-1e-3, 1e-3,51); % Probe plane points in Z 
 
 s_rad = 1e-3; % define the sample radius (where the particles will actually be
 con = 0.7; 
 
 %Save outputs
-[SaveVar4p2] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
-SaveVar4p2.timer = toc; SaveVar4p2.comments = "Looking at 2xZ again, this time with linear, 10001 point probe line";
+[SaveVar1p2] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
+SaveVar1p2.timer = toc; SaveVar1p2.comments = "Same as before, 10001 probe in z, double theta rsolution";
 
