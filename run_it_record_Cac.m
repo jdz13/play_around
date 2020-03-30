28eb45d6c709d6d06e1c0c4870467f2fcc102029
jdz13 <jdz25@cam.ac.uk>
2020-03-30 15:17:18 +0100

    addition of 6.p2 into var space. Updated the record too

--- run_it_record_Cac.m
+++ run_it_record_Cac.m
@@ -135,3 +135,138 @@ SaveVar4p1.timer = toc; SaveVar4p1.comments = "YZ convergence testing, 2xZ";
 %'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 %%
+
+% God I was bad here, I'll sort it out.
+
+    %% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
+
+tic
+
+theta = linspace(0,pi/2,91); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = [5,4,3,2.5,2]; % Key ratio values, how strict of a condition do we want 
+RES = [0.15,0.2,0.25,0.3,0.35,0.4]; % Start field values. 
+
+pm_cl = [2,3,4]*1e-2; % Magnet outer diameters.
+
+Yin = linspace(-1e-3, 1e-3,101); % Probe plane points in Y 
+Zin = linspace(-1e-3, 1e-3,51); % Probe plane points in Z 
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; 
+
+%Save outputs
+[SaveVar2p2] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
+SaveVar2p2.timer = toc; SaveVar2p2.comments = "Looking at this again, this time with linear, 10001 point probe line";
+%% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
+
+tic
+
+theta = linspace(0,pi/2,91); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = [5,4,3,2.5,2]; % Key ratio values, how strict of a condition do we want 
+RES = [0.15,0.2,0.25,0.3,0.35,0.4]; % Start field values. 
+
+pm_cl = [2,3,4]*1e-2; % Magnet outer diameters.
+
+Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
+Zin = linspace(-1e-3, 1e-3,101); % Probe plane points in Z 
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; 
+
+%Save outputs
+[SaveVar4p2] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
+SaveVar4p2.timer = toc; SaveVar4p2.comments = "Looking at 2xZ again, this time with linear, 10001 point probe line";
+%% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
+
+clear
+
+tic
+
+theta = linspace(0,pi/2, 181); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = [5,4,3,2.5,2]; % Key ratio values, how strict of a condition do we want 
+RES = [0.15,0.2,0.25,0.3,0.35,0.4]; % Start field values. 
+
+pm_cl = [2,3,4]*1e-2; % Magnet outer diameters.
+
+Yin = linspace(-1e-3, 1e-3,101); % Probe plane points in Y 
+Zin = linspace(-1e-3, 1e-3,51); % Probe plane points in Z 
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; 
+
+%Save outputs
+[SaveVar1p2] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
+SaveVar1p2.timer = toc; SaveVar1p2.comments = "Same as before, 10001 probe in z, double theta rsolution";
+
+%% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
+
+clear
+
+tic
+
+theta = linspace(0,pi/2,1081); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = 2.5; % Key ratio values, how strict of a condition do we want 
+RES = [0.15,0.2,0.25,0.3,0.35,0.4]; % Start field values. 
+
+pm_cl = 4e-2; % Magnet outer diameters.
+
+Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
+Zin = linspace(-1e-3, 1e-3,51); % Probe plane points in Z 
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; 
+
+%Save outputs
+[SaveVar6p0] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
+SaveVar6p0.timer = toc; SaveVar6p0.comments = "Increasing angular resolution (1081) heavily to probe relationship";
+
+%% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
+
+clear
+
+tic
+
+theta = linspace(0,pi/2,5401); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = 2.5; % Key ratio values, how strict of a condition do we want 
+RES = [0.15,0.2,0.25,0.3,0.35,0.4]; % Start field values. 
+
+pm_cl = 4e-2; % Magnet outer diameters.
+
+Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
+Zin = linspace(-1e-3, 1e-3,51); % Probe plane points in Z 
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; 
+
+%Save outputs
+[SaveVar6p1] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
+SaveVar6p1.timer = toc; SaveVar6p1.comments = "Increasing angular resolution (5401) heavily to probe relationship";
+
+%% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
+
+clear
+
+tic
+
+theta = linspace(0,pi/2,90001); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = 2.5; % Key ratio values, how strict of a condition do we want 
+RES = [0.15,0.2,0.25,0.3,0.35,0.4]; % Start field values. 
+
+pm_cl = 4e-2; % Magnet outer diameters.
+
+Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
+Zin = linspace(-1e-3, 1e-3,51); % Probe plane points in Z 
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; 
+
+%Save outputs
+[SaveVar6p2] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
+SaveVar6p2.timer = toc; SaveVar6p2.comments = "Increasing angular resolution (90001) heavily to probe relationship";
+
