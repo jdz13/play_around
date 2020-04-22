f9a23cd1253ea42dab00d88ba7cb332a07c3e157
jdz13 <jdz25@cam.ac.uk>
2020-04-22 15:39:54 +0100

    new L data, to look into on laptop

--- run_it_Cac.m
+++ run_it_Cac.m
@@ -1,36 +1,24 @@
 %% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
 
+clear
 
 tic
 
 theta = linspace(0,pi/2,9001); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
 
-KRV = [5,4,3,2.5]; % Key ratio values, how strict of a condition do we want 
-RES = 0.2; % Start field values. 
+KRV = 5; % Key ratio values, how strict of a condition do we want 
+RES = 0.4; % Start field values. 
 
-pm_cl = 4e-2; % Magnet outer diameters.
+pm_cl = linspace(2,4,11).*1e-2; % Magnet outer diameters.
 
 s_rad = 1e-3; % define the sample radius (where the particles will actually be
 con = 0.7; 
 
 Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
-Zin = Yin; % Probe plane points in Z 
-
-% 
-% %Save outputs
-% [SaveVar9p1] = search_tool_1_Caciagli(KRV,RES,pm_cl,theta,Yin,Zin,s_rad,con);
-% SaveVar9p1.timer = toc; SaveVar9p1.comments = "Running the code again to check the addition of L into the loops, this one is using V3 as a base data set for comparison";
+Zin = Yin(1:26); % Probe plane points in Z 
 
 Lengths = pm_cl;
 
 %Save outputs
-[SaveVar9p2] = search_tool_3_Caciagli(KRV,RES,pm_cl,Lengths, theta,Yin,Zin,s_rad,con);
-SaveVar9p2.timer = toc; SaveVar9p2.comments = "Running the code again to check the addition of L into the loops, this one is using V3 as a base data set for comparison";
-
-
-Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
-Zin = Yin(1:26); % Probe plane points in Z 
-
-%Save outputs
-[SaveVar9p3] = search_tool_4_Caciagli(KRV,RES,pm_cl,Lengths, theta,Yin,Zin,s_rad,con);
-SaveVar9p3.timer = toc; SaveVar9p3.comments = "Checking the V2 script, this is using Yin(1:26), so no redefinition and should be no FPP error";
+[SaveVar10p2] = search_tool_4_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
+SaveVar10p2.timer = toc; SaveVar10p2.comments = "Starting to look into the L, PMcl links, constraining start field and KRV";
---
+++ saved_var_plotter_V4_comparison.m
@@ -0,0 +1,49 @@
+function [] = saved_var_plotter_V4_comparison (ist,figno,MxB)
+% ist is the input structure, designed from the new function
+% should only now need that input and what figure you would like and it
+% will sort the rest. 
+
+thrsh = MxB(3,size(MxB,2)); %Why is this a 3? I should change this. 
+% If we want the smallest value from any magnet, we could use min(MxB(MxB > 0),[],'all') 
+% Else if we would like the smallest field from the biggest magnet , we could use max(MxB(:,size(MxB,2),:),[],'all')
+
+plotter = zeros(length(ist.varst.KRV), length(ist.varst.PM), length(ist.varst.RES),length(ist.varst.Lengths));
+str = inputname(1);
+
+for i = 1:length(ist.varst.KRV)
+    for j = 1:length(ist.varst.PM)
+        for k = 1:length(ist.varst.RES)
+            for l = 1:length(ist.varst.Lengths)
+                plotter(i,j,k,l) = nnz(ist.SWres(i,:,j, k, l)>thrsh);
+            end 
+        end 
+    end 
+end
+
+if length(ist.varst.RES) == 1
+    
+    nox = 1;
+    noy = 1;
+else 
+    nox = 2;
+
+    if rem(length(ist.varst.RES),2) ==1
+        noy = length(ist.varst.RES)+1/2;
+    else 
+        noy = length(ist.varst.RES)/2;
+    end 
+end
+
+KRVno = 1;
+
+figure(figno); clf;
+    for jj = 1:length(ist.varst.RES)
+        subplot(nox,noy,jj); imagesc(ist.varst.PM, ist.varst.Lengths, squeeze(plotter(KRVno,:,jj,:)));
+        xlabel 'PM size [cm]'; ylabel 'Lengths'; title (['Start field = ', num2str(ist.varst.RES(jj)),'T'])
+        caxis([min(plotter,[],'all'),max(plotter,[],'all')]); colorbar
+        
+        if jj == ceil(noy/2)
+            title (compose("Data for " + str + "\nKRV  = [" + num2str(ist.varst.KRV) + "], Con = " + num2str(ist.varst.CON) + "\n" + ist.comments + "\n \n Start field = " + num2str(ist.varst.RES(jj)) + "T"))
+        end 
+    end
+end
