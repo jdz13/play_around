9a1eafaade0785415aa2629acc3cfe127daa1091
jdz13 <jdz25@cam.ac.uk>
2021-02-03 12:44:33 +0000

    changes to run it reflexting the new tests for chV

--- run_it_Cac.m
+++ run_it_Cac.m
@@ -47,3 +47,38 @@ s_rad = 1e-3;
 RES = linspace(0.6,0.1,51); % Start field values. 
 [SaveVar22p4] = search_tool_7p2_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
 SaveVar22p4.timer = toc; SaveVar22p4.comments = "Start field scan, 51 point linescan";
+
+%%
+tic
+theta = linspace(0,pi/2,9001); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = [5]; % Key ratio values, how strict of a condition do we want 
+RES = [0.4]; % Start field values. 
+
+pm_cl = linspace(2,10,81).*1e-2; % Magnet outer diameters.
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; % condition to be applied to FWHM - 0.7 = 70/30 condition. 
+
+Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
+Zin = Yin(1:26); % Probe plane points in Z 
+
+Lengths = 4e-2; % Magnet lengths
+
+
+%Save outputs
+[SaveVar22p5] = search_tool_7p3_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
+SaveVar22p5.timer = toc; SaveVar22p5.comments = "second magnet OD scan, checking 7p3 validation";
+
+%%
+tic
+
+Yin = linspace(-1e-3, 1e-3,101); % Probe plane points in Y 
+Zin = Yin(1:51);
+KRV = 5;
+RES = 0.4;
+Lengths = 4e-2; % Magnet lengths
+pm_cl = 4e-2; % Magnet outer diameters.
+s_rad = linspace(0.25,1,61).*1e-3;
+[SaveVar22p6] = search_tool_7p3_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
+SaveVar22p6.timer = toc; SaveVar22p6.comments = "2nd sample radius scan, 61 point linescan. double resolution. Using new tool adapted for function, will need a validation test.";
