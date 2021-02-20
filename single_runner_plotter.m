382b2e40ff00eb6fb1c25501519d3d3f21a3f407
Jake Zipfel <jdz25@cam.ac.uk>
2021-02-20 12:14:17 +0000

    getting the merged files in properly. All should be good now. Header lines assessed and removed.

--- single_runner_plotter.m
+++ single_runner_plotter.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 %%
 
 % generate the data
@@ -76,83 +75,4 @@ ylabel 'NSA';
 set(h,'Xtick', sort(xtickvals))
 vline(xtickvals)
 set(h,'Ytick', [0,0.5,1])
-=======
-%%
-
-% generate the data
-single_runner
-%% Plot it up. Can use method 1 (tester4) or method 2 (tester2).
-
-% define which method you want to look at.
-ist = tester4;
-
-% extract the relevant information.
-maxes = rad2deg(nonzeros(ist.MLOC));
-widths = rad2deg(nonzeros(ist.FWHMres));
-xtheta = rad2deg(ist.varst.theta);
-NSAdata = ist.masterNVC;
-
-% plot it up nicely.
-jo = figure; subplot(2,1,1)
-plot(xtheta, NSAdata);
-h = gca;vline(maxes)
-legendCell = cellstr(num2str(widths, 'W_0_._3_-_0_._7 = %-g [deg]')); 
-legend(legendCell, 'Location', 'Northwest')
-xlim([0,90])
-vline(maxes+(KRV*widths),'b:')
-xlabel '\theta_m [deg]'
-ylabel 'NSA'
-set(h,'Xtick', maxes)
-set(h,'Ytick', [0,0.5,1])
-thesis_fig_gen(jo.Number)
-
-Bmax = zeros (1, length(theta));
-for pull = 1:length(theta)
-    Bxnew = Xunitx.*cos(theta(pull)) + Yunitx.*sin(theta(pull)); 
-    Bmax(pull) = max(Bxnew, [], 'all');
-end 
-    % plot it up nicely.
-subplot(2,1,2)
-plot(Bmax, NSAdata);
-h = gca;
-legendCell = cellstr(num2str(widths, 'W_0_._3_-_0_._7 = %-g [deg]')); 
-legend(legendCell, 'Location', 'Northwest')
-xlabel 'Feild [T]'
-ylabel 'NSA';[~,col] = find(theta == nonzeros(ist.MLOC));
-set(h,'Xtick', fliplr(Bmax(col)))
-vline(Bmax(col))
-set(h,'Ytick', [0,0.5,1])
-thesis_fig_gen(jo.Number)
-
-%%
-
-% define which method you want to look at.
-ist = tester2;
-
-% extract the relevant information.
-maxes = rad2deg(nonzeros(ist.MLOC));
-widths = rad2deg(nonzeros(ist.FWHMres));
-fields = (nonzeros(ist.SWres));
-xtheta = rad2deg(ist.varst.theta);
-NSAdata = ist.masterNVC;
-xfields = ist.B_maxes(:,1:size(maxes,1))';
-xtickvals = zeros(1, size(maxes,1));
-[row,col] = find(theta == nonzeros(ist.MLOC));
-
-% plot it up nicely.
-jo = figure; 
-for gg = 1:size(maxes,1)
-    plot(xfields(gg,:), NSAdata(gg,:));
-    hold on
-    xtickvals(gg) = xfields(row(gg),col(gg));
-end 
-h = gca;
-legendCell = cellstr(num2str(widths, 'W_0_._3_-_0_._7 = %-g [deg]')); 
-legend(legendCell, 'Location', 'Northwest')
-xlabel 'Feild [T]'
-ylabel 'NSA';
-set(h,'Xtick', sort(xtickvals))
-vline(xtickvals)
-set(h,'Ytick', [0,0.5,1])
->>>>>>> refs/remotes/origin/master
 thesis_fig_gen(jo.Number)
--- thesis_ch6_figures.m
+++ thesis_ch6_figures.m
@@ -182,8 +182,6 @@ clear jj kk pp
 
 %%
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 load('C:\Users\JDZ\Documents\Thesis\Code Outputs\Chapter V\matlab_SV23p5.mat')
 
 ist = SaveVar23p5;
@@ -203,9 +201,6 @@ title 'Number of possible channels'; thesis_fig_gen(ff.Number);
 
 %%
 
-=======
-=======
->>>>>>> refs/remotes/origin/master
 x = linspace(0, 90,181);
 y = cosd(x);
 h1 = figure; subplot(1,2,1); plot(x,y)
@@ -260,11 +255,3 @@ set(h,'Ytick', [0,0.3,0.5,0.7,1])
 annotation('arrow',[0.59,0.48],[0.85,0.85])
 annotation('arrow',[0.48,0.59],[0.85,0.85])
 text (17.2,0.82,'FWHM')
-
-
-<<<<<<< HEAD
-%%
->>>>>>> 36dd8f2c88d50a79b62a05998dba0f17cb935a6c
-=======
-%%
->>>>>>> refs/remotes/origin/master
