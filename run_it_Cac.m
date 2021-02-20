6b2da083bd406e567ee5152dba5d88e0dd4d6d24
jdz13 <jdz25@cam.ac.uk>
2021-02-20 11:13:04 +0000

    Taking out the merge headers. This was used to create the new  chapter V and VI data, i.e. SV24p1-4 and SV25p1-4. Seems to be running smoothly, outputting a saved variable at each line.

--- run_it_Cac.m
+++ run_it_Cac.m
@@ -6,7 +6,7 @@ tic
 
 theta = linspace(0,pi/2,9001); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
 
-KRV = 5; % Key ratio values, how strict of a condition do we want 
+KRV = 10; % Key ratio values, how strict of a condition do we want 
 RES = 0.4; % Start field values. 
 
 pm_cl = linspace(2,10,81).*1e-2; % Magnet outer diameters.
@@ -19,10 +19,11 @@ Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y
 Zin = Yin(1:26); % Probe plane points in Z 
 
 tic
-[SaveVar24p1] = search_tool_8p1_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con, sigmoid_pc);
+[SaveVar24p1] = search_tool_8p1_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
 SaveVar24p1.timer = toc; SaveVar24p1.comments = "Capturing the same data as in 22p1 but on new code";
 
-folpath = 'C:\Users\JDZ\Documents\Thesis\Code Outputs\Chapter V';
+% folpath = 'C:\Users\JDZ\Documents\Thesis\Code Outputs\Chapter V';
+folpath = 'C:\Users\Jake\Documents\MATLAB';
 [oldfolder] = cd(folpath);
 filename = 'matlab_SV24p1.mat';
 save(filename, 'SaveVar24p1')
@@ -31,7 +32,7 @@ cd(oldfolder);
 tic
 Lengths = linspace(2,10,81).*1e-2; % Magnet lengths
 pm_cl = 4e-2; % Magnet outer diameters.
-[SaveVar24p2] = search_tool_8p1_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con, sigmoid_pc);
+[SaveVar24p2] = search_tool_8p1_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
 SaveVar24p2.timer = toc; SaveVar24p2.comments = "Capturing the same data as in 22p2 but on new code";
 
 [oldfolder] = cd(folpath);
@@ -70,7 +71,7 @@ tic
 
 theta = linspace(0,pi/2,9001); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
 
-KRV = 5; % Key ratio values, how strict of a condition do we want 
+KRV = 10; % Key ratio values, how strict of a condition do we want 
 RES = 0.4; % Start field values. 
 
 pm_cl = linspace(2,10,81).*1e-2; % Magnet outer diameters.
@@ -78,6 +79,7 @@ Lengths = 4e-2; % Magnet lengths
 
 s_rad = 1e-3; % define the sample radius (where the particles will actually be
 con = 0.7; % condition to be applied to FWHM - 0.7 = 70/30 condition. 
+sigmoid_pc = 5; 
 
 Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
 Zin = Yin(1:26); % Probe plane points in Z 
@@ -85,8 +87,8 @@ Zin = Yin(1:26); % Probe plane points in Z
 tic
 [SaveVar25p1] = search_tool_8p2_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con, sigmoid_pc);
 SaveVar25p1.timer = toc; SaveVar25p1.comments = "Capturing the same data as in 23p1 but on new code";
-
-folpath = 'C:\Users\JDZ\Documents\Thesis\Code Outputs\Chapter V';
+%%
+folpath = 'C:\Users\Jake\Documents\MATLAB';
 [oldfolder] = cd(folpath);
 filename = 'matlab_SV25p1.mat';
 save(filename, 'SaveVar25p1')
@@ -108,7 +110,7 @@ RES = 0.4;
 Lengths = 4e-2; % Magnet lengths
 pm_cl = 4e-2; % Magnet outer diameters.
 s_rad = linspace(0.25,1,31).*1e-3;
-[SaveVar25p3] = search_tool_8p2_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
+[SaveVar25p3] = search_tool_8p2_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con, sigmoid_pc);
 SaveVar25p3.timer = toc; SaveVar25p3.comments = "Capturing the same data as in 22p3 but on new code";
 
 [oldfolder] = cd(folpath);
@@ -121,7 +123,7 @@ Lengths = 4e-2; % Magnet lengths
 pm_cl = 4e-2; % Magnet outer diameters.
 s_rad = 1e-3;
 RES = linspace(0.6,0.1,51); % Start field values. 
-[SaveVar25p4] = search_tool_8p2_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
+[SaveVar25p4] = search_tool_8p2_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con, sigmoid_pc);
 SaveVar25p4.timer = toc; SaveVar25p4.comments = "Start field scan, 51 point linescan";
 
 [oldfolder] = cd(folpath);
