71b28c4b0409ac20f049d6e5689804c4a7c657c3
jdz13 <jdz25@cam.ac.uk>
2021-03-24 11:15:58 +0000

    changes to a couple of functions to get 2D sig study running properly for the right KRV

--- run_it_Cac.m
+++ run_it_Cac.m
@@ -1,6 +1,5 @@
 %% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
 
-% clear
 
 %%
 tic
@@ -10,36 +9,22 @@ theta = linspace(0,pi/2,9001); % define the angular resolution. Only up to 90 de
 KRV = 10; % Key ratio values, how strict of a condition do we want 
 RES = 0.4; % Start field values. 
 
-pm_cl = 4e-2; % Magnet outer diameters.
-Lengths = 4e-2; % Magnet lengths
+pm_cl = linspace(2,10,21).*1e-2; % Magnet outer diameters.
+Lengths = pm_cl; % Magnet lengths
 
 s_rad = 1e-3; % define the sample radius (where the particles will actually be
 con = 0.7; % condition to be applied to FWHM - 0.7 = 70/30 condition. 
+sigmoid_pc = 5; 
 
 Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
 Zin = Yin(1:26); % Probe plane points in Z 
 
-a = inf;
-
 tic
-[SaveVar30p8] = search_tool_10p2a_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con,a);
-SaveVar30p8.timer = toc; SaveVar30p8.comments = "testing the sigmoid decay parameter a = inf. Does this give the ideal results? ";
+[SaveVar30p10] = search_tool_10p2_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
+SaveVar30p10.timer = toc; SaveVar30p10.comments = "2D OD L scan with KRV = 10. For Ch VI";
 
-folpath = 'C:\Users\Jake\Documents\MATLAB';
 [oldfolder] = cd(folpath);
-filename = 'matlab_SV30p8.mat';
-save(filename, 'SaveVar30p8')
+filename = 'matlab_SV30p10.mat';
+save(filename, 'SaveVar30p10')
 cd(oldfolder);
 % ------------------------------------------------------------------------
-%%
-a = logspace(-2,3,51);
-
-tic
-[SaveVar30p9] = search_tool_10p2a_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con,a);
-SaveVar30p9.timer = toc; SaveVar30p9.comments = "testing the sigmoid decay parameter a, does it converge to ideal results? ";
-
-folpath = 'C:\Users\Jake\Documents\MATLAB';
-[oldfolder] = cd(folpath);
-filename = 'matlab_SV30p9.mat';
-save(filename, 'SaveVar30p9')
-cd(oldfolder);
--- run_it_record_Cac.m
+++ run_it_record_Cac.m
@@ -1582,3 +1582,30 @@ folpath = 'C:\Users\Jake\Documents\MATLAB';
 filename = 'matlab_SV30p9.mat';
 save(filename, 'SaveVar30p9')
 cd(oldfolder);
+
+%%
+tic
+
+theta = linspace(0,pi/2,9001); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = 10; % Key ratio values, how strict of a condition do we want 
+RES = 0.4; % Start field values. 
+
+pm_cl = linspace(2,10,21).*1e-2; % Magnet outer diameters.
+Lengths = pm_cl; % Magnet lengths
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; % condition to be applied to FWHM - 0.7 = 70/30 condition. 
+
+Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
+Zin = Yin(1:26); % Probe plane points in Z 
+
+tic
+[SaveVar29p10] = search_tool_8p1_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
+SaveVar29p10.timer = toc; SaveVar29p10.comments = "2D OD L scan with KRV = 10. For Ch VI";
+
+[oldfolder] = cd(folpath);
+filename = 'matlab_SV29p10.mat';
+save(filename, 'SaveVar29p10')
+cd(oldfolder);
+% ------------------------------------------------------------------------
