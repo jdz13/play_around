4a8c13f2c6c939b2eb3e711247b445a32c9a13fe
jdz13 <jdz25@cam.ac.uk>
2021-03-29 23:36:55 +0100

    minor changes to run_it. Committing so that it can be edited on the laptop

--- run_it_record_Cac.m
+++ run_it_record_Cac.m
@@ -1640,3 +1640,35 @@ filename = 'matlab_SV30p10.mat';
 save(filename, 'SaveVar30p10')
 cd(oldfolder);
 % ------------------------------------------------------------------------
+%% Run it. Script for controlling entry variables into the new Caciagli code, ready for analysis.
+
+% clear
+
+%%
+tic
+
+theta = linspace(0,pi/2,9001); % define the angular resolution. Only up to 90 degrees, symmetry conditions help after.
+
+KRV = 10; % Key ratio values, how strict of a condition do we want 
+RES = 0.4; % Start field values. 
+
+pm_cl = linspace(2,10,81).*1e-2; % Magnet outer diameters.
+Lengths = 4e-2; % Magnet lengths
+
+s_rad = 1e-3; % define the sample radius (where the particles will actually be
+con = 0.7; % condition to be applied to FWHM - 0.7 = 70/30 condition. 
+
+Yin = linspace(-1e-3, 1e-3,51); % Probe plane points in Y 
+Zin = Yin(1:26); % Probe plane points in Z 
+
+tic
+[SaveVar31p1] = search_tool_11p1_Caciagli(KRV,RES,pm_cl,Lengths,theta,Yin,Zin,s_rad,con);
+SaveVar31p1.timer = toc; SaveVar31p1.comments = "Running the ideal study - with no suppression (working range or minimum separation). OD study";
+
+folpath = 'C:\Users\JDZ\Documents\Thesis\Code Outputs\Chapter V';
+% folpath = 'C:\Users\Jake\Documents\MATLAB';
+[oldfolder] = cd(folpath);
+filename = 'matlab_SV31p1.mat';
+save(filename, 'SaveVar31p1')
+cd(oldfolder);
+% -----------------------
